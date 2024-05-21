1cccbc051565b0d4409241f9b440e160d734e12c
sayan4545 <sayanchatterjee3010@gmail.com>
2024-05-22 03:44:28 +0530

    Encoded password

--- src/main/java/uberbackend/uberprojectauthservice/configurations/SpringSecurity.java
+++ src/main/java/uberbackend/uberprojectauthservice/configurations/SpringSecurity.java
@@ -5,12 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SpringSecurity  {
-    public void configuration(HttpSecurity http) throws Exception {
-
+public class SpringSecurity {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf->csrf.disable()).authorizeHttpRequests(auth->auth.requestMatchers("/api/v1/auth/signUp/*")
+                .permitAll())
+                .build();
     }
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
